0b29c3e53092149d3dbc03af654409fe7dfd628e
Frederik Reich <frederikreich@googlemail.com>
2021-01-25 14:36:11 +0100

    Add files via upload

---
+++ programming-basics/1_language/0_comments.js
@@ -0,0 +1,26 @@
+// This is a one-liner
+
+// These are multiple one-liners
+// 1
+// 2
+// 3
+
+/*
+    This is a
+    multiline comment
+*/
+
+/***
+ * This is a JSDoc comment
+ * wich looks
+ * very
+ * clean
+ * and
+ * adds
+ * a
+ * star
+ * at
+ * every
+ * new
+ * line
+ */

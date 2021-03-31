fb298182f68f92b59711a8c1e2e7b2c99e0db4a0
FRickReich <frickreich@gmail.com>
2021-03-31 10:57:35 +0200

    Add modules-import-export

---
+++ single-page-application/2_modules/1_import-export/user.js
@@ -0,0 +1,8 @@
+// es gibt noch eine andere art des imports, mit der wir alle werte, funktionne und klassen als namespace holen können
+
+const firstName = "Max";
+const lastName = "Mustermann";
+const age = 25;
+const roles = [ "Member", "Admin" ]
+
+export { firstName, lastName, age, roles }

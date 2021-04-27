5840d2374d67d5c593ef148c8ebddcbe1a452baf
FRickReich <frickreich@gmail.com>
2021-04-27 11:12:06 +0200

    Initial for components app

---
+++ single-page-application/5_components/components-app/src/App.js
@@ -0,0 +1,20 @@
+import { Header } from './components';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header/>
+
+      <main>
+        Unser Inhalt...
+      </main>
+
+      <footer>
+        Unser Footer
+      </footer>
+    </>
+  );
+}
+
+export default App;
---
+++ single-page-application/5_components/components-app/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+
+const Header = () =>
+{
+    return(
+        <header className="Header">
+            <h1>Unsere Webseite</h1>
+        </header>
+    );
+}
+
+export default Header;

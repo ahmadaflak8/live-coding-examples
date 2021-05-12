484722b2e995a1265d4542359b5f2d76d9d2fb06
FRickReich <frickreich@gmail.com>
2021-05-12 13:33:47 +0200

    Add solution for editing

--- single-page-application/5_components/todo-app/src/App.js
+++ single-page-application/5_components/todo-app/src/App.js
@@ -1,130 +1,160 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
-
 function App()
 {
-  const [ tasks, setTasks ] = useState([]);
-  const [ inputText, setInputText ] = useState("");
-
-  // Lade vorhandene tasks aus der localstorage
-  useEffect(() =>
-  {
-      const savedTasks = window.localStorage.getItem('tasks');
-
-      setTasks(JSON.parse(savedTasks));
-  }, []);
-
-  // Speichere aktuelle tasks in die localstorage
-  useEffect(() =>
-  {
-      const currentTasks = JSON.stringify(tasks);
-      window.localStorage.setItem('tasks', currentTasks);
-  }, [tasks]);
-
-  // Wichtig, erst laden, dann speichern.
-
-  /**
-   * @method createId();
-   * @description Gibt eine zufällige ID aus.
-   * @returns { string }
-   */
-  const createId = () =>
-  {
-    return Math.random().toString(16).substr(2, 8);
-  }
-
-  /**
-   * @method addTask();
-   * @description Fügt eine neue Task ein
-   */
-  const addTask = () =>
-  {
-    const newTask =
+    const [tasks, setTasks] = useState([]);
+    const [inputText, setInputText ] = useState("");
+    const [ selectedTaskText, setSelectedTaskText ] = useState("");
+    useEffect(() =>
     {
-      id: createId(),
-      text: inputText,
-      isFinished: false
+        const oldTasks = JSON.parse(window.localStorage.getItem('tasks'));
+        oldTasks.map(task =>
+        {
+            task.isEditable = false;
+            return { ...task }
+        });
+        setTasks(oldTasks);
+    }, []);
+    useEffect(() =>
+    {
+        const currentTasks = JSON.stringify(tasks);
+        window.localStorage.setItem('tasks', currentTasks);
+    }, [tasks]);
+    /**
+     * @method createId();
+     * @description Gibt eine zufällige ID zurück.
+     * @returns { string }
+     */
+    const createId = () =>
+    {
+        return Math.random().toString(16).substr(2, 8); // 254e82e3
     }
-
-    console.log(newTask);
-
-    setTasks(tasks => [...tasks, newTask]);
-
-    setInputText("");
-  }
-
-  /**
-   * @method toggleTask();
-   * @description Schaltet den zustand eines tasks um.
-   * @param { object } e
-   * @param { string } id
-   */
-  const toggleTask = (e, id) =>
-  {
-    const currentTask = tasks.filter(task => task.id === id)[0];
-    const otherTasks = tasks.filter(task => task.id !== id);
-
-    currentTask.isFinished = !currentTask.isFinished;
-
-    setTasks([...otherTasks, currentTask]);
-  }
-
-  /**
-   * @method deleteTask();
-   * @description Löscht ein element aus der task array anhand der übergebenen ID
-   * @param { object } e
-   * @param { string } id
-   */
-  const deleteTask = (e, id) =>
-  {
-      setTasks(tasks => tasks.filter(task => task.id !== id));
-  }
-
-  /**
-   * @method handleNewTaskInput();
-   * @description Behandelt die eingabe des input elements.
-   * @param { object } e 
-   */
-  const handleNewTaskInput = (e) =>
-  {
-    setInputText(e.target.value);
-  }
-
-  return (
-    <div className="App">
-      <h1>Todo App</h1>
-
-      <h3>Neue Aufgabe</h3>
-
-      <input type="text" onChange={ handleNewTaskInput } value={ inputText }></input>
-      <button onClick={ addTask }>Eintrag hinzufügen</button>
-
-      <h3>Aufgaben</h3>
-
-      {
-        tasks.length > 0 ?
-        <ul>
-          {
-            tasks.map((task, i) =>
+    /**
+     * @method addTask();
+     * @description Fügt eine neue task ein.
+     */
+    const addTask = () =>
+    {
+        const newTask = {
+            id: createId(),
+            text: inputText,
+            isFinished: false,
+            isEditable: false
+        }
+        console.log(newTask);
+        setTasks(tasks => [...tasks, newTask]);
+        setInputText("");
+    }
+    /**
+     * @method toggleTask();
+     * @description Schaltet den zustand eines tasks um.
+     * @param { object } e
+     * @param { string } id
+     */
+    const toggleTask = (e, id) =>
+    {
+        const currentTask = tasks.filter(task => task.id === id)[0];
+        const otherTasks = tasks.filter(task => task.id !== id);
+        currentTask.isFinished = !currentTask.isFinished;
+        if(currentTask.isFinished === true)
+        {
+            setTasks([...otherTasks, currentTask ]);
+        }
+        else
+        {
+            setTasks([currentTask, ...otherTasks ]);
+        }
+    }
+    /**
+     * @method deleteTask();
+     * @description Löscht ein element aus der task array anhand der übergebenen ID
+     * @param { object } e
+     * @param { string } id
+     */
+    const deleteTask = (e, id) =>
+    {
+        setTasks(tasks => tasks.filter(task => task.id !== id));
+    }
+    /**
+     * @method handleNewTaskInput();
+     * @description Behandelt die eingabe des input elements.
+     * @param { object } e 
+     */
+    const handleNewTaskInput = (e) =>
+    {
+        setInputText(e.target.value);
+    }
+    const toggleHandleEdit = (e, id) =>
+    {
+        const currentTasks = tasks.map(task =>
+        {
+            const selectedTask = task.id === id ? task.isEditable = true : task.isEditable = false;
+            return { ...task, selectedTask };
+        });
+        setSelectedTaskText(() => tasks.filter(task => task.id === id)[0].text);
+        setTasks(() => currentTasks);
+    }
+    const saveTask = (e, id) =>
+    {
+        const currentTasks = tasks.map(task =>
             {
-              return(
-                <li key={ i } className={ task.isFinished ? 'finished' : 'active' }>
-                  <span className="taskText" onClick={ (e) => toggleTask(e, task.id) }>
-                    { task.text }
-                  </span>
-                  <button onClick={ (e) => deleteTask(e, task.id) }>x</button>
-                </li>
-              )
-            }).reverse()
-          }
-        </ul>
-        :
-        <p className="noTasksMessage">Keine Aufgaben vorhanden</p>
-      }
-
-
-    </div>
-  );
+                if(task.id === id)
+                {
+                    task.text = selectedTaskText;
+                    task.isEditable = false;
+                }
+                return { ...task };
+        });
+        setTasks(() => currentTasks);
+    }
+    return (
+        <div className="App">
+            <h1>Todo App</h1>
+            <h3>Neue Aufgabe</h3>
+            <div className="addTaskSection">
+                <input onChange={ handleNewTaskInput } type="text" value={ inputText }></input>
+                <button className="addButton" onClick={ addTask }>Add</button>
+            </div>
+            <h3>Aufgaben</h3>
+            {
+                tasks.length > 0 ?
+                <ul>
+                    {
+                        tasks.map((task, i) =>
+                        {
+                            return (
+                                <li key={ i } className={ task.isFinished ? 'finished' : 'active' }>
+                                    {
+                                        task.isEditable ? 
+                                        (
+                                            <div className="editTaskSection">
+                                                <input value={ selectedTaskText } onChange={ (e) => setSelectedTaskText(e.target.value) }/>
+                                                <button className="saveButton" onClick={(e) => saveTask(e, task.id)}>&#x2714;</button>
+                                                <button className="deleteButton" onClick={(e) => deleteTask(e, task.id)}>X</button>
+                                            </div>
+                                        )
+                                        :
+                                        (
+                                            <div className="viewTaskSection">
+                                                <span className="taskText" onClick={ (e) => toggleTask(e, task.id) }>
+                                                { 
+                                                    task.text
+                                                }
+                                                </span>
+                                                <button className="editButton" onClick={ (e) => toggleHandleEdit(e, task.id)}>&#x270E;</button>
+                                                <button className="deleteButton" onClick={(e) => deleteTask(e, task.id)}>X</button>
+                                            </div>
+                                        )
+                                    }
+                                </li>
+                            )
+                        }).reverse()
+                    }
+                </ul>
+                :
+                <p className="noTasksMessage">Keine Aufgaben vorhanden</p>
+            }
+        </div>
+    );
 }
-
 export default App;

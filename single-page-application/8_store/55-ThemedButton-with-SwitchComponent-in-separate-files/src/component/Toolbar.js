41e883457cadd22f4421385fe4a4a0bee746281a
Thomas Hofmann <thomas@savoy-truffle.de>
2021-05-20 10:17:40 +0200

    code formatting

--- single-page-application/8_store/55-ThemedButton-with-SwitchComponent-in-separate-files/src/component/Toolbar.js
+++ single-page-application/8_store/55-ThemedButton-with-SwitchComponent-in-separate-files/src/component/Toolbar.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import { ThemeContext } from '../App';
 
-
 function Toolbar() {
-    return (
-      <div>
-        <ThemedButton />
-      </div>
-    );
+  return (
+    <div>
+      <ThemedButton />
+    </div>
+  );
 }
 
 function ThemedButton() {
-return (<Button />);
+  return (<Button />);
 }
 
 function Button() {
-const themeContext = React.useContext(ThemeContext);
-return (<button className={themeContext}>Themed Button ({themeContext})</button>);
+  const themeContext = React.useContext(ThemeContext);
+  return (<button className={themeContext}>Themed Button ({themeContext})</button>);
 }
 
 export default Toolbar
-//export { Toolbar };
